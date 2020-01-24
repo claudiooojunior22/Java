32900511ce009afa96f9afd64c60e2d4f8e115f4
Claudio Luiz dos Santos Junior <48703166+claudiooojunior22@users.noreply.github.com>
2020-01-24 17:06:20 -0300

    Initial commit

---
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author ASP
+ */
+public class JavaApplication5 {
+int funcRec(int x, int y)
+      if (x==1)
+      return y;
+      return 1 + funcRec(x-1, x*y);
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+    
+}
---
+++ JavaApplication5/src/javaapplication5/x.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author ASP
+ */
+class x {
+    int funcRec(int 4, int 3)
+      if (x==1)
+      return 4;
+      return 1 + funcRec(4-1, 4*3);
+    /**
+}
